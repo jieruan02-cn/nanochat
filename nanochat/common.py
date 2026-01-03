f8c12aacfa3b8bab09adf70f4667073573f91e33
jieruan <jieruan@didi-labs.com>
2026-01-03 19:23:10 +0800

    Add dataloader.py.

--- nanochat/common.py
+++ nanochat/common.py
@@ -10,4 +10,20 @@ import torch
 import torch.distributed as dist
 from filelock import FileLock
 
+def is_ddp_requested() -> bool:
+  """
+  True if launched by torchrun (env present), even before init.
+  Used to decide whether we *should* initalized a PG (process group).
+  """
+  return all(k in os.environ for k in ("RANK", "LOCAL_RANK", "WORLD_SIZE"))
+
+def get_dist_info():
+  if is_ddp_requested():
+    assert all(var in os.environ for var in ["RANK", "LOCAL_RANK", "WORLD_SIZE"])
+    ddp_rank = int(os.environ['RANK'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])
+    ddp_world_size = int(os.environ['WORLD_SIZE'])
+    return True, ddp_rank, ddp_local_rank, ddp_world_size
+  else:
+    return False, 0, 0, 1
 
